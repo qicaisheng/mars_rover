732a4fa0fcf3af75e0228222a6aa55d9f7793f9b
qicaisheng <qicaisheng@qq.com>
2020-05-29 14:54:42 +0800

    add leftDirection and rightDirection to Direction enum

--- src/main/java/com/twschool/practice/marsrover/Direction.java
+++ src/main/java/com/twschool/practice/marsrover/Direction.java
@@ -1,7 +1,47 @@
 package com.twschool.practice.marsrover;
 
 public enum Direction {
-    NORTH("N"), WEST("W"), SOUTH("S"), EAST("E");
+    NORTH("N") {
+        @Override
+        public Direction leftDirection() {
+            return WEST;
+        }
+
+        @Override
+        public Direction rightDirection() {
+            return EAST;
+        }
+    }, WEST("W") {
+        @Override
+        public Direction leftDirection() {
+            return SOUTH;
+        }
+
+        @Override
+        public Direction rightDirection() {
+            return NORTH;
+        }
+    }, SOUTH("S") {
+        @Override
+        public Direction leftDirection() {
+            return EAST;
+        }
+
+        @Override
+        public Direction rightDirection() {
+            return WEST;
+        }
+    }, EAST("E") {
+        @Override
+        public Direction leftDirection() {
+            return NORTH;
+        }
+
+        @Override
+        public Direction rightDirection() {
+            return SOUTH;
+        }
+    };
 
     private String shortName;
 
@@ -14,7 +54,7 @@ public enum Direction {
         return shortName;
     }
 
-    public Direction leftDirection() {
-        return WEST;
-    }
+    public abstract Direction leftDirection();
+    
+    public abstract Direction rightDirection();
 }
--- src/test/java/com/twschool/practice/marsrover/DirectionTest.java
+++ src/test/java/com/twschool/practice/marsrover/DirectionTest.java
@@ -9,8 +9,42 @@ public class DirectionTest {
 
     @Test
     public void should_return_the_WEST_when_get_left_direction_given_direction_NORTH() {
-        Direction north = Direction.NORTH;
-
-        Assert.assertEquals(Direction.WEST, north.leftDirection());
+        Assert.assertEquals(Direction.WEST, Direction.NORTH.leftDirection());
     }
+
+    @Test
+    public void should_return_the_EAST_when_get_right_direction_given_direction_NORTH() {
+        Assert.assertEquals(Direction.EAST, Direction.NORTH.rightDirection());
+    }
+
+    @Test
+    public void should_return_the_SOUTH_when_get_left_direction_given_direction_WEST() {
+        Assert.assertEquals(Direction.SOUTH, Direction.WEST.leftDirection());
+    }
+    
+    @Test
+    public void should_return_the_NORTH_when_get_right_direction_given_direction_WEST() {
+        Assert.assertEquals(Direction.NORTH, Direction.WEST.rightDirection());
+    }
+    
+    @Test
+    public void should_return_the_EAST_when_get_left_direction_given_direction_SOUTH() {
+        Assert.assertEquals(Direction.EAST, Direction.SOUTH.leftDirection());
+    }
+
+    @Test
+    public void should_return_the_WEST_when_get_right_direction_given_direction_SOUTH() {
+        Assert.assertEquals(Direction.WEST, Direction.SOUTH.rightDirection());
+    }
+
+    @Test
+    public void should_return_the_NORTH_when_get_left_direction_given_direction_EAST() {
+        Assert.assertEquals(Direction.NORTH, Direction.EAST.leftDirection());
+    }
+
+    @Test
+    public void should_return_the_SOUTH_when_get_right_direction_given_direction_EAST() {
+        Assert.assertEquals(Direction.SOUTH, Direction.EAST.rightDirection());
+    }
+
 }
