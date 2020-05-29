0aa5897b47e8cfc58737336ccf6c0005cbb696cd
qicaisheng <qicaisheng@qq.com>
2020-05-29 15:34:10 +0800

    extract Coordinates class

---
+++ src/main/java/com/twschool/practice/marsrover/Coordinates.java
@@ -0,0 +1,19 @@
+package com.twschool.practice.marsrover;
+
+public class Coordinates {
+    private int coordinatesX;
+    private int coordinatesY;
+
+    public Coordinates(int coordinatesX, int coordinatesY) {
+        this.coordinatesX = coordinatesX;
+        this.coordinatesY = coordinatesY;
+    }
+
+    public int getCoordinatesX() {
+        return coordinatesX;
+    }
+
+    public int getCoordinatesY() {
+        return coordinatesY;
+    }
+}
